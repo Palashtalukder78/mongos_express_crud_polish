47ac4fbf334abfe79820f0599dd38cf29a2b0bde
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-22 20:44:09 +0600

    completing signin process with jwt(jsonwebtoken) and use dotenv for hide important things

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const userSchema = require("../schemas/userSchema");
 
 //Make user model
 const User = new mongoose.model("User", userSchema);
 
+//SignUp
 router.post("/signup", async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
   try {
@@ -22,6 +25,48 @@ router.post("/signup", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       error: "Signup Failed!",
+    });
+  }
+});
+
+//Signin
+router.post("/signin", async (req, res) => {
+  try {
+    const user = await User.find({ username: req.body.username });
+    if (user && user.length > 0) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user[0].password
+      );
+      if (isValidPassword) {
+        //Generate web token
+        var token = jwt.sign(
+          {
+            username: user[0].username,
+            userId: user[0]._id,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
+        res.status(200).json({
+          accessToken: token,
+          message: "Login successfully!",
+        });
+      } else {
+        res.status(401).json({
+          error: "Authentication failed!",
+        });
+      }
+    } else {
+      res.status(401).json({
+        error: "Authentication failed!",
+      });
+    }
+  } catch {
+    res.status(401).json({
+      error: "Authentication failed!",
     });
   }
 });
